ab5d50dfa8339d2356bffd3440457ad908fec621
jasonravagli <jasonravagli@gmail.com>
2021-11-03 16:07:22 +0100

    experiment with retry

--- gui/src/test/java/it/jasonravagli/gym/gui/RetryOnUbuntuRule.java
+++ gui/src/test/java/it/jasonravagli/gym/gui/RetryOnUbuntuRule.java
@@ -1,7 +1,7 @@
 package it.jasonravagli.gym.gui;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.rules.TestRule;
 import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
